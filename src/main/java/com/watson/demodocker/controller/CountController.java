0af12c720092d5400a010ab2d5fefd021e1eba9f
Watsonsci <watsonsci@hotmail.com>
2024-04-28 13:51:24 -0400

    First Commit

---
+++ src/main/java/com/watson/demodocker/controller/CountController.java
@@ -0,0 +1,19 @@
+package com.watson.demodocker.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CountController {
+    @Autowired
+    StringRedisTemplate redisTemplate;
+
+    @GetMapping("/hello")
+    public String count(){
+        Long increment = redisTemplate.opsForValue().increment("count-people");
+        return increment+ " already visited this website.";
+    }
+
+}
